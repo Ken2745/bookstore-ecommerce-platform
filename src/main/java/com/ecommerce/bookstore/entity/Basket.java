e2f96659a6efb513bd77ddedd6ac52e315e80e33
Ken2745 <kensherrod14@gmail.com>
2026-02-26 00:32:08 -0500

    entities

---
+++ src/main/java/com/ecommerce/bookstore/entity/Basket.java
@@ -0,0 +1,20 @@
+package com.ecommerce.bookstore.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Data
+@RedisHash("BasketItem")
+public class Basket {
+
+    @Id
+    private Integer id;
+    private String name;
+    private String description;
+    private Long price;
+    private String pictureUrl;
+    private String productBrand;
+    private String productType;
+    private Integer quantity;
+}
